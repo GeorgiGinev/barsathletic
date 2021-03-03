a67ed08194a510366d7d15eb5f5bb1b23a86448a
g.ginev <gosho.ginev@gmail.com>
2021-03-03 12:29:22 +0200

    Basic setup

---
+++ header.php
@@ -0,0 +1,11 @@
+<!doctype html>
+<html <?php language_attributes(); ?>>
+<head>
+    <meta charset="<?php bloginfo( 'charset' ); ?>" />
+    <meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no" />
+    <?php wp_head(); ?>
+
+    <link rel="stylesheet" type="text/css" href="<?php bloginfo('stylesheet_directory'); ?>style.css" />
+</head>
+<body <?php body_class(); ?>>
+<?php wp_body_open(); ?>
