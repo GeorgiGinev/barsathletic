71738c05990c721e291d6fbd0fd6a71182831edd
g.ginev <gosho.ginev@gmail.com>
2021-04-26 22:38:39 +0300

    added contacts template

---
+++ contacts.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Template Name: Contact template
+ */
+
+get_header(); ?>
+
+    <main class="main-body">
+        <?php
+        include_once('views/layout/contacts.php');
+        ?>
+    </main>
+
+<?php get_footer(); ?>
