35b65ade0befad2e548420da88638265af478252
g.ginev <gosho.ginev@gmail.com>
2021-08-06 17:39:30 +0300

    style css update

--- modules/modules.php
+++ modules/modules.php
@@ -12,8 +12,8 @@ include_once('tgm/tgm-module.php');
  */
 function theme_name_scripts()
 {
-    wp_enqueue_style('style', get_template_directory_uri().'/style.css', '', '1.0.0' );
-    wp_enqueue_script('script', get_template_directory_uri().'/public/js/app.js', array('jquery'), null, true);
+    wp_enqueue_style('barsathletic', get_template_directory_uri().'/style.css', '', '1.0.0' );
+    wp_enqueue_script('barsathletic', get_template_directory_uri().'/public/js/app.js', array('jquery'), null, true);
 }
 
 add_action('wp_enqueue_scripts', 'theme_name_scripts');
